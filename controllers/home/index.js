c37d719acca32dfb2aa180313a9a11a8cb02a501
John Barker <jdabarker@gmail.com>
2021-07-09 19:06:07 +0800

    updated home route

--- controllers/home/index.js
+++ controllers/home/index.js
@@ -5,7 +5,7 @@ const fs = require("fs/promises");
 router.get("/", async (req, res) => {
   const pathToFile = path.join(__dirname, "../../db/count.txt");
   const count = await fs.readFile(pathToFile, "utf-8");
-  const newCount = (parseInt(count) + 1).toString();
+  const newCount = parseInt(count) + 1;
   await fs.writeFile(pathToFile, newCount.toString());
   res.status(200).send(newCount);
 });
